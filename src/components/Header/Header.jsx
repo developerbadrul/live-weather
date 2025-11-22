75231fbf60d537b4e429500111f960e248748e6c
Md Badrul Alam <developer.badrul@gmail.com>
2025-11-22 21:52:38 +0600

    add Header Component

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,21 @@
+import Favourite from "./Favourite";
+import FavouriteLocationModal from "./FavouriteLocationModal";
+import Logo from "./Logo";
+import Search from "./Search";
+
+const Header = () => {
+    return (
+        <header className="fixed w-full top-0 z-50 bg-linear-to-b from-black/60 to-black/0 pb-10">
+            <nav className="container flex items-center justify-between py-6">
+                <Logo />
+                <div className="flex items-center gap-4 relative">
+                    <Search />
+                    <Favourite />
+                    <FavouriteLocationModal />
+                </div>
+            </nav>
+        </header>
+    );
+};
+
+export default Header;
