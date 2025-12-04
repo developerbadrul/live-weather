e05a0eb8d68e59c497f1574fcb7cc7a89cbf8c46
Md Badrul Alam <developer.badrul@gmail.com>
2025-12-04 21:46:37 +0600

    add dynamic bg according climate

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,8 +2,42 @@ import Header from "../components/Header/Header";
 import WeatherBoard from "../components/Weather/WeatherBoard";
 import useWeather from "../hooks/useWeather";
 
+import ClearSkyImage from "../assets/backgrounds/clear-sky.jpg";
+import FewCloudsImage from "../assets/backgrounds/few-clouds.jpg";
+import MistImage from "../assets/backgrounds/mist.jpeg";
+import RainyDayImage from "../assets/backgrounds/rainy-day.jpg";
+import ScatterdCloudsImage from "../assets/backgrounds/scattered-clouds.jpg";
+import SnowImage from "../assets/backgrounds/sunny.jpg";
+import ThunderStormImage from "../assets/backgrounds/thunderstorm.jpg";
+import WinterImage from "../assets/backgrounds/winter.jpg";
+
 const Home = () => {
-    const { loading, error, requestBrowserLocation } = useWeather();
+    const { loading, error, requestBrowserLocation, weatherData } = useWeather();
+
+    function getBackgroundImage(climate) {
+        switch (climate) {
+            case "Rain":
+                return RainyDayImage;
+            case "Clouds":
+                return ScatterdCloudsImage;
+            case "Clear":
+                return ClearSkyImage;
+            case "Snow":
+                return SnowImage;
+            case "Thunder":
+                return ThunderStormImage;
+            case "Fog":
+                return WinterImage;
+            case "Haze":
+                return FewCloudsImage;
+            case "Mist":
+                return MistImage;
+            default:
+                return ClearSkyImage;
+        }
+    }
+
+    const dynamicBg = getBackgroundImage(weatherData.climate)
 
     if (loading.state) {
         return (
@@ -39,7 +73,7 @@ const Home = () => {
                 <p className="text-3xl text-emerald-500 font-semibold">{loading.message}</p>
             </div>
         ) : (
-            <div className="grid place-items-center h-screen">
+            <div style={{ backgroundImage: `url(${dynamicBg})` }} className="grid place-items-center h-screen bg-no-repeat bg-cover">
                 <Header />
                 <main>
                     <section>
